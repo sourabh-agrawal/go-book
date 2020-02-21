8a592f54f3a886ab8324523f65429110b78b44db
Sourabh Agrawal <sourabh181097@gmail.com>
2020-02-21 22:04:36 +0530

    Chapter 4

---
+++ chapter4/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+var (
+	no float64
+)
+
+func main() {
+	var x string
+	x = "Hello"
+	fmt.Println(x)
+	x += " World"
+	fmt.Println(x)
+
+	// String comparison
+	var a string = "Hello"
+	var b string = "Hello"
+	fmt.Println(a == b)
+
+	// shorhands
+	z := "Hello World"
+	fmt.Println(z)
+
+	var y = "Hello World"
+	fmt.Println(y)
+
+	dogsName := "Max"
+	fmt.Println("My dog's name is", dogsName)
+
+	// constants
+	const c = "Hello World"
+	fmt.Println(c)
+
+	// Example program
+	fmt.Print("Enter a number: ")
+	fmt.Scanf("%f", &no)
+	output := no * 2
+	fmt.Println("You entered: ", no, " and the double of it is ", output)
+}
